0c8f8ffbd613ce564bf9bad7886bb95eb25eb136
unknown <czimmer@czimmer.(none)>
2012-10-18 13:01:51 -0400

    made some temporary controls to show how puzzle generation and puzzle solving work

--- Sudoku/Sudoku/obj/Debug/Sudoku_Content.g.i.cs
+++ Sudoku/Sudoku/obj/Debug/Sudoku_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("puzzles/test2.txt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("puzzles/test3.txt")]
 
 
